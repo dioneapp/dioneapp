09a026a6544ed535ae8db02970de2b69e23d77c1
blaisewf <blaise@applio.org>
2025-01-19 12:28:43 +0100

    fix: :bug: update sidebar links to use React Router's Link component

--- src/renderer/src/components/layout/sidebar.tsx
+++ src/renderer/src/components/layout/sidebar.tsx
@@ -10,13 +10,13 @@ export default function Sidebar() {
             <div className="absolute -top-10 -left-14 bg-[#BCB1E7] blur-3xl w-64 h-64 rounded-full rounded-bl-none rounded-tl-none opacity-40" />
             <div className="flex flex-col items-center justify-start h-full w-full p-4 z-50 px-6">
                 <div className="w-full h-44 flex flex-col justify-center items-start gap-2">
-                    <a
-                        href="/"
+                    <Link
+                        to={"/"}
                         className="flex gap-2 hover:opacity-80 transition-opacity"
                     >
                         <img src={dio} alt="Dione logo" className="h-8 w-8" />
                         <h1 className="font-semibold text-3xl">Dione</h1>
-                    </a>
+                    </Link>
                     <p className="text-xs text-neutral-400 px-0.5">Explore, Install, Innovate — in 1 Click.</p>
                     <div className="mt-2 w-full flex gap-2 px-0.5">
                         <button onClick={() => openLink("https://getdione.app/discord")} className="text-xs w-full bg-white hover:bg-white/80 transition-colors duration-400 rounded-full text-black font-semibold py-1 text-center">Discord</button>
