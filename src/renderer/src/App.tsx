0407698fd8def14e68262e89ebda0ec3f00ba5bc
bygimenez <davidalbal123@gmail.com>
2025-01-25 14:21:09 +0100

    fix: fix scrollbar on install page

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Loading from "./pages/loading";
 import Titlebar from "./components/layout/titlebar";
 import Home from "./pages/home";
@@ -8,13 +8,15 @@ import Settings from "./pages/settings";
 import { ToastProvider } from "./utils/useToast";
 
 function App() {
+  const { pathname } = useLocation();
+
   return (
     <ToastProvider>
     <div className="h-screen w-screen">
       <Titlebar />
       <div className="flex h-full">
         <Sidebar />
-        <div className="flex-1 overflow-auto mt-6">
+        <div className={`flex-1 mt-6 ${pathname === '/' ? 'overflow-auto' : 'overflow-hidden'}`}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/loading" element={<Loading />}/>
