c37026e7e0dcbfa59b0f7bd4792dec634931a873
bygimenez <davidalbal123@gmail.com>
2025-02-01 09:35:57 +0100

    refactor: minor changes

--- src/main/server/scripts/runner.ts
+++ src/main/server/scripts/runner.ts
@@ -2,71 +2,10 @@ import * as fs from 'fs';
 import path from 'path';
 import { Server } from 'socket.io';
 import logger from '../utils/logger';
-import * as semver from 'semver';
-import { Action, DependencyChecks } from './types';
+import { Action } from './types';
 import { execute } from './execute';
 import { createEnvironment, executeOnEnvironment, existsEnvironment } from './environment';
-
-// check dependencies commands
-const dependencyCheckers: {[key: string]: DependencyChecks} = {
-    git: {
-        command: 'git --version',
-        versionRegex: /git version (\d+\.\d+\.\d+)/,
-    },
-    python: {
-        command: 'python --version',
-        altCommand: 'python3 --version',
-        versionRegex: /Python (\d+\.\d+\.\d+)/,
-    },
-    conda: {
-        command: 'conda --version',
-        versionRegex: /conda (\d+\.\d+\.\d+)/,
-    },
-    node: {
-        command: 'npm --version',
-        versionRegex: /(\d+\.\d+\.\d+)/,
-    }
-}
-// check if dependency is installed
-const checkDependency = async (
-    depName: string,
-    requiredVersion: string,
-    io: Server,
-    workingDir: string
-): Promise<void> => {
-    const checker = dependencyCheckers[depName];
-    if (!checker) {
-        io.emit("installUpdate", { type: 'error', content: `Error: Dependency '${depName}' is not suported` });
-        logger.error(`Error: Dependency '${depName}' is not suported`);
-    }
-
-    let version: string | null = null
-    try {
-        let output = await execute(checker.command, io, workingDir, "dependency") || ''
-        const match = output.match(checker.versionRegex);
-        if (match) version = match[1];
-
-        // try alt command
-        if (!version && checker.altCommand) {
-            output = await execute(checker.altCommand, io, workingDir, "dependency") || ''
-            const altMatch = output.match(checker.versionRegex);
-            if (altMatch) version = altMatch[1];
-        }
-    } catch (error) {
-        io.emit("installUpdate", { type: 'error', content: `Error checking dependency '${depName}': ${error}` });
-        logger.error(`Error checking dependency '${depName}': ${error}`);
-    }
-    if (!version) {
-        io.emit("installUpdate", { type: 'error', content: `Error: Dependency '${depName}' not found` });
-        logger.error(`Error: Dependency '${depName}' not found`);
-    }
-    if (!semver.satisfies(version, requiredVersion)) {
-        io.emit("installUpdate", { type: 'error', content: `Error: Dependency '${depName}' version ${version} does not satisfy ${requiredVersion}` });
-        logger.error(`Error: Dependency '${depName}' version ${version} does not satisfy ${requiredVersion}`);
-    }
-
-    io.emit("installUpdate", {type: 'log', content: `✓ Dependency '${depName}' found.`});
-}
+import { checkDependency } from './dependencies';
 
 // execute actions
 const executeActions = async (actions: Action, io: Server, workingDir: string) => {
@@ -85,26 +24,27 @@ const executeActions = async (actions: Action, io: Server, workingDir: string) =
     if (actions.installation) {
         io.emit('installUpdate', { type: 'status', status: 'pending', content: 'Installing...' });
         for (const step of actions.installation) {
+            // check if env exists
             if (step.env) {
                 const exists = await existsEnvironment(step.env, workingDir)
                 logger.warn(`${step.env} dont exists in ${workingDir}`)
                 if (!exists) {
+                    // create env
                     io.emit(`Creating environment '${step.env}' on '${workingDir}'`)
                     logger.info('Creating environment')
                     await createEnvironment(step.env, workingDir, io)
                 }
             }
             for (const cmd of step.commands) { 
-                io.emit('installUpdate', { 
-                    type: 'log', 
-                    content: `${step.name}: ${cmd}` 
-                });
+                io.emit('installUpdate', {type: 'log', content: `${step.name}: ${cmd}`});
 
                 if (cmd.startsWith('cd')) {
+                    // change directory
                     const newDir = cmd.split(' ')[1];
                     workingDir = path.resolve(workingDir, newDir); 
                     io.emit('installUpdate', { type: 'log', content: `Actual dir: ${workingDir}` });
                 } else if (step.env) {
+                    // execute on env if exists
                     await executeOnEnvironment(cmd, step.env, workingDir, io)
                 } else  {
                     await execute(cmd, io, workingDir);
@@ -128,19 +68,23 @@ export const startScript = async (workingDir: string, io: Server) => {
         if (actions.start) {
             io.emit('installUpdate', { type: 'status', status: 'pending', content: 'Starting script...' });
             for (const command of actions.start) {
+                // catch port if exists
                 if (command.catch) {
                     logger.info(`Executing script on: http://127.0.0.1://${command.catch}`);
                     io.emit('installUpdate', { type: 'catch', content: command.catch });
                     io.emit('installUpdate', { type: 'log', content: `Watching port ${command.catch}` });
                 }
+                // execute commands sequentially 
                 for (const cmd of command.commands) {
                     if (cmd.startsWith('cd')) {
+                        // change directory
                         io.emit('installUpdate', { type: 'log', content: `Changing directory to '${cmd.split(' ')[1]}'` });
                         const newDir = cmd.split(' ')[1];
                         workingDir = path.join(workingDir, newDir);
                     } else {
                         io.emit('installUpdate', { type: 'log', content: `${command.name}` });
                         if (command.env) {
+                            // execute on env if exists
                             await executeOnEnvironment(cmd, command.env, workingDir, io)
                         } else  {
                             await execute(cmd, io, workingDir);
@@ -149,36 +93,13 @@ export const startScript = async (workingDir: string, io: Server) => {
                 }
             }
         } else {
+            // not found start action
             io.emit('installUpdate', { type: 'error', content: 'No start action found' });
         }
     } catch (error) {
         console.error('Error starting script:', error);
     }
 }
-
-// stop script, probably dont need this
-// export const stopScript = async (workingDir: string, io: Server) => {
-//     try {
-//         io.emit('installUpdate', 'Stopping script...');
-//         const dioneFile = path.join(workingDir, 'dione.json');
-//         const data = fs.readFileSync(dioneFile, 'utf8');
-//         const actions: Action = JSON.parse(data);
-//         // read stop action
-//         if (actions.stop) {
-//             io.emit('installUpdate', 'Stopping...');
-//             for (const command of actions.stop) {
-//                 io.emit('installUpdate', `${command.name}`);
-//                 for (const cmd of command.commands) {
-//                     await execute(cmd, io, workingDir);
-//                 }
-//             }
-//         }
-//     } catch (error) {
-//         console.error('Error stopping script:', error);
-//     }
-
-//     io.emit('installUpdate', { type: 'log', content: 'Process exited with code 1' });
-// };
 
 // uninstall script
 export const uninstallScript = async (workingDir: string, io: Server) => {
