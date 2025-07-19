490b15d67d9726558dcf8b8927f017ceb6c9829e
deeivihh <deeivihh@hotmail.com>
2025-07-19 09:30:34 +0200

    Add a small gradient effect in `sidebar.tsx`

--- src/renderer/src/components/layout/sidebar.tsx
+++ src/renderer/src/components/layout/sidebar.tsx
@@ -79,8 +79,9 @@ export default function Sidebar() {
 
 	return (
 		<div
-			className={`flex flex-col items-center justify-center border-r border-white/10 overflow-hidden ${config?.compactMode ? "max-w-24 w-24" : "max-w-70 w-70"}`}
+			className={`relative flex flex-col items-center justify-center border-r border-white/10 overflow-hidden ${config?.compactMode ? "max-w-24 w-24" : "max-w-70 w-70"}`}
 		>
+			<div className="absolute bottom-0 bg-gradient-to-t rounded-full -mb-24 blur-3xl from-white to-transparent w-full h-24"/>
 			<div className="absolute -top-10 -left-14 bg-[#BCB1E7] blur-3xl max-w-64 w-full h-64 rounded-full rounded-bl-none rounded-tl-none opacity-40" />
 			<div className="flex flex-col items-center justify-start h-full w-full p-4 z-50 px-6">
 				<div
