d80a154e67998608d9eb9b5dca42e50d73a9b07d
deeivihh <deeivihh@hotmail.com>
2025-11-11 17:58:54 +0100

    Add `read_file` tool to AI + better styles

---
+++ src/main/server/routes/ai/ollama/tools.ts
@@ -0,0 +1,37 @@
+import fs from 'node:fs'
+import path from 'node:path';
+import logger from '../../../utils/logger';
+
+const availableTools = [
+    {
+        type: 'function',
+        function: {
+            name: 'read_file',
+            description: 'Read and return contents of the given file path',
+            parameters: {
+                type: 'object',
+                properties: {
+                    file_path: {
+                        type: 'string',
+                        description: 'The path to the file to read'
+                    }
+                },
+                required: ['file_path']
+        }
+        }
+    }
+]
+
+export function getTools() {
+	return availableTools
+}
+
+export function readFile(file_name: string, workspacePath: string) {
+    const full_path = path.normalize(path.join(workspacePath, file_name))
+    if (!fs.existsSync(full_path)) {
+		return 'File not found'
+	}
+    logger.ai(`Reading file: ${full_path}`) 
+	const fileContents = fs.readFileSync(full_path, 'utf8');
+    return fileContents
+}
