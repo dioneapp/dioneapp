e61cc553fc463eaf709519da8671f0f586f437fc
deeivihh <deeivihh@hotmail.com>
2025-11-12 18:36:15 +0100

    Fix `read_file` tool

--- src/main/server/routes/ai/ollama/tools.ts
+++ src/main/server/routes/ai/ollama/tools.ts
@@ -29,9 +29,20 @@ export function getTools() {
 export function readFile(file_name: string, workspacePath: string) {
 	const full_path = path.normalize(path.join(workspacePath, file_name));
 	if (!fs.existsSync(full_path)) {
-		return "File not found";
+		// if file not found in workspace path, search for it
+		const files = fs.readdirSync(workspacePath);
+		const foundFile = files.find((file) => file === file_name);
+		if (foundFile) {
+			const foundPath = path.join(workspacePath, foundFile);
+			logger.ai(`Reading file: ${foundPath}`);
+			const fileContents = fs.readFileSync(foundPath, "utf8");
+			return fileContents;
+		}
+	} else {
+		logger.ai(`Reading file: ${full_path}`);
+		const fileContents = fs.readFileSync(full_path, "utf8");
+		return fileContents;
 	}
-	logger.ai(`Reading file: ${full_path}`);
-	const fileContents = fs.readFileSync(full_path, "utf8");
-	return fileContents;
+	logger.ai(`File not found: ${full_path}`);
+	return "File not found";
 }
